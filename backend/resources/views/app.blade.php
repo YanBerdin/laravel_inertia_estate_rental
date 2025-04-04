147e28ebcd97c2199fb5ee17c0d8f4b884e73573
YanBerdin <yandevformation@gmail.com>
2025-04-05 01:39:44 +0200

    Fix Sass and PHP 7.3 compatibility issue in Laravel 8 by reverting to .css file

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <!--@vite('resources/js/app.js') -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/style.css') }}" rel="stylesheet">
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
 </head>
--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -18,5 +18,7 @@ const mix = require("laravel-mix");
  */
 
 mix.js("resources/js/app.js", "public/js")
-    .sass("resources/css/style.scss", "public/css")
+    .postCss('resources/css/style.css', 'public/css')
+    // 
+    //.sass('resources/sass/style.scss', 'public/css')
     .react();
